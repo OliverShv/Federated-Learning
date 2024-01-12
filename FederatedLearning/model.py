1f839a76486b74e3ae51a7ec6c66b73ec357860c
OliverShv <oliver.shave@Gmail.com>
2024-01-12 17:55:45 +0000

    Gradient calculations added
    Model class manually calculates gradients

--- FederatedLearning/model.py
+++ FederatedLearning/model.py
@@ -4,13 +4,30 @@ import json
 class Model:
     def __init__(self):
         self.model = None
-        self.complier_settings = None
-        self.fit_settings = None
+        self.loss_function = None
+        self.optimiser = None
 
-    def string_to_model(self, model_str):
+        self.avg_graidents= None
+        self.gradients = None
+        self.gradients_applied = 0
+
+    async def load_model(self, model_str):
         model_json = json.load(model_str)
         self.model = tf.keras.load(model_json)
+    
+    async def apply_weights(self, weights_str):
+        pass
 
-    def train(self):
+    async def calculate_graidents(self, x = None , y = None):
         self.model.compile(**self.complier_settings)
-        self.model.fit(**self.fit_settings)
+        
+        with tf.GradientTape() as tape:
+            # Forward pass
+            predictions = self.model(x)
+            loss = self.loss_function(y, predictions)
+
+        # Calculate gradients with respect to every trainable variable
+        gradients = tape.gradient(loss, self.model.trainable_variables)
+
+    async def apply_graidents(self, gradients):
+        self.optimizer.apply_gradients(zip(gradients, self.model.trainable_variables))
